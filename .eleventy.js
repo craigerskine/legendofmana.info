32e1a2440b6fff4d3f7b5082cc837047ee1e4079
Craig - qrayg <craig@craigerskine.com>
2023-02-24 21:59:55 -0600

    11ty 2.0

--- .eleventy.js
+++ .eleventy.js
@@ -1,17 +1,15 @@
 const { EleventyRenderPlugin } = require("@11ty/eleventy");
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+const esbuild = require("esbuild");
 const markdownIt = require("markdown-it");
 const markdownItAttrs = require("markdown-it-attrs");
 const yaml = require("js-yaml");
 
 module.exports = function(eleventyConfig) {
 
-  eleventyConfig.addPassthroughCopy('_site/_redirects');
-  eleventyConfig.addPassthroughCopy('_site/_assets');
-  eleventyConfig.addPassthroughCopy('_site/*.ico');
-  eleventyConfig.addPassthroughCopy('_site/*.png');
-  eleventyConfig.addPassthroughCopy('_site/*.txt');
-  eleventyConfig.addPassthroughCopy('_site/site.webmanifest');
+  eleventyConfig.addPassthroughCopy('_site/_assets/img');
+  eleventyConfig.addPassthroughCopy('_site/_assets/files');
+  eleventyConfig.addPassthroughCopy({'_site/_assets/_root': './'});
   
   eleventyConfig.addPlugin(EleventyRenderPlugin);
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
@@ -44,15 +42,30 @@ module.exports = function(eleventyConfig) {
     });
     return pageArr.slice(0, limit);
   });
+
+  // pluck
   eleventyConfig.addFilter('pluck', function (arr, value, attr) {
     return arr.filter((item) => item[attr] === value);
   });
+  
   // for item in (items | flatMap('category') | unique('category'))
   eleventyConfig.addFilter('flatMap', (list, key) => list.flatMap((x) => x[key]));
   eleventyConfig.addFilter('unique', (list, key) => {
     const map = new Map(list.map((x) => [x[key], x]))
     return [...map.values()]
   });
+
+  // esbuild
+  eleventyConfig.on('eleventy.before', async () => {
+    await esbuild.build({
+      entryPoints: ['_site/_app/_app.js'],
+      outfile: 'public/_assets/js/_app.js',
+      bundle: true,
+      minify: true,
+      sourcemap: false,
+    });
+  });
+
   return {
     jsDataFileSuffix: '.data',
     markdownTemplateEngine: 'njk',
