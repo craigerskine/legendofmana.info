9a688e0a0a0f52122a90ea1bbde9c727da97eb8f
Craig @craigerskine @qrayg <craig@craigerskine.com>
2025-04-10 10:01:37 -0500

    tw config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,5 @@
-module.exports = {
-  content: ["/**/*.{html,js,njk,md}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+/*
+  [x] requirement for intelliSense
+  [x] https://marketplace.visualstudio.com/items?itemName=bradlc.vscode-tailwindcss
+  [x] intentionally empty
+*/
